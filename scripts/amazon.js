29d6cc1c984731fb02d42c48142fd1ff33cff04a
rudrasankarg <rudrasankarg@gmail.com>
2025-12-28 04:03:02 +0530

    Use product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,4 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    price: 299
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4.0,
-        count: 127
-    },
-    price: 999
-}, {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    price: 350
-}];
-
 let productsHTML = '';
-
-
 
 products.forEach((product) => {
     productsHTML += `
