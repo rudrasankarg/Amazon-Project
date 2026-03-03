decc1a2109e7df1be9c124941e429d4801d76bcf
rudrasankarg <rudrasankarg@gmail.com>
2026-03-04 01:01:21 +0530

    Made cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -62,7 +62,7 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
-        
+
         let matchingItem;
 
         cart.forEach((item) => {
@@ -78,5 +78,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
             quantity: 1
           });
         }
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => { 
+        cartQuantity += item.quantity;
+      });
+
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
