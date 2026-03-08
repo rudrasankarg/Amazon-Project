a5439de17bc96249369079f47efd831831d1b756
rudrasankarg <rudrasankarg@gmail.com>
2026-03-09 03:32:17 +0530

    XHR to Fetch request

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,16 +2,12 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { cart, loadCart } from "../data/cart.js";
 
 
 Promise.all([
-    new Promise((resolve) => {
-    loadProducts(() => {
-        resolve();
-    });
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
             resolve();
